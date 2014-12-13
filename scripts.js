b935f22a5318bcc14367be64ad80e14a1647b319
liamjmc <liamjmc@gmail.com>
2014-12-13 13:29:49 +0000

    Added photos. Font-weight on hover

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 $("#navigation-hover").mouseover(function (){
            $("#navigation-background").animate({width: "100%"}, 500);
-            
+            $("#menu-list li").css("font-weight", "bolder");
            });
 
         $("#navigation-hover").mouseleave(function (){
             $("#navigation-background").animate({width: "0"}, 500);
+	    $("#menu-list li").css("font-weight", "100");
            });
