6c27537bbd026e5666dcb346093c3db509d2afc6
liamjmc <liamjmc@gmail.com>
2014-12-13 19:34:24 +0000

    Added image gallery

--- scripts.js
+++ scripts.js
@@ -7,3 +7,8 @@ $("#navigation-hover").mouseover(function (){
             $("#navigation-background").animate({width: "0"}, 500);
 	    $("#menu-list li").css("font-weight", "100");
            });
+
+function makeBackground(img) {
+	$("body").css("background", "url(" + img.src + ") no-repeat");
+    $("body").css("background-size", "cover");
+}
