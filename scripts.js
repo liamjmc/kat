933794ad93bf30c13a6629c40eba71d6a4571f7e
liamjmc <liamjmc@gmail.com>
2014-12-14 01:30:36 +0000

    Added social network buttons and fixed menu size.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$("#navigation-hover").mouseover(function (){
+/*$("#navigation-hover").mouseover(function (){
            $("#navigation-background").animate({width: "100%"}, 500);
             $("#menu-list li").css("font-weight", "bolder");
     $("#name a").css({'font-weight' : '900'});
@@ -8,10 +8,8 @@ $("#navigation-hover").mouseover(function (){
             $("#navigation-background").animate({width: "0"}, 500);
 	    $("#menu-list li").css("font-weight", "100");
             $("#name a").css({'font-weight' : '900'});
-           });
+           });*/
 
-
-var aboutActive = false;
 
 $("#about-link").click(function() {
     $("#about").show("slow");
@@ -20,6 +18,31 @@ $("#about-link").click(function() {
 $("#x").click(function() {
     $("#about").hide("slow");
 });
+
+window.onload = addListeners();
+
+function addListeners(){
+    document.getElementById('about').addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+}
+
+function mouseUp()
+{
+    window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e){
+  window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e){
+    var div = document.getElementById('about');
+    div.style.marginTop = 0;
+  div.style.position = 'absolute';
+  div.style.top = e.clientY + 'px';
+  div.style.left = e.clientX + 'px';
+}
 
 function makeBackground(img) {
 	$("body").css("background", "url(" + img.src + ") no-repeat");
